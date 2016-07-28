68e27d7311ee882169bd74aa4995d2969c5e711d
ttan <ttan@husky.ca>
2016-07-28 15:55:56 +0800

    refactored example.cpp - renamed and grouped examples to several namespaces - added class remote_target to manage remote info at once - added class auto_handle to manage handles in the RAII way

--- example/example.cpp
+++ example/example.cpp
@@ -3,213 +3,268 @@
 
 #include <iostream>
 
-static void NotifyCallback(const AmsAddr* pAddr, const AdsNotificationHeader* pNotification, uint32_t hUser)
+struct remote_target
 {
-    const uint8_t* data = reinterpret_cast<const uint8_t*>(pNotification + 1);
-    std::cout << "hUser 0x" << std::hex << hUser <<
-        " sample time: " << std::dec << pNotification->nTimeStamp <<
-        " sample size: " << std::dec << pNotification->cbSampleSize <<
-        " value:";
-    for (size_t i = 0; i < pNotification->cbSampleSize; ++i) {
-        std::cout << " 0x" << std::hex << (int)data[i];
-    }
-    std::cout << '\n';
-}
+    const char* ip_v4;
+    AmsAddr server;
+};
 
-uint32_t getHandleByNameExample(std::ostream& out, long port, const AmsAddr& server,
-                                const std::string handleName)
+namespace examples
 {
-    uint32_t handle = 0;
-    const long handleStatus = AdsSyncReadWriteReqEx2(port,
-                                                     &server,
-                                                     ADSIGRP_SYM_HNDBYNAME,
-                                                     0,
-                                                     sizeof(handle),
-                                                     &handle,
-                                                     handleName.size(),
-                                                     handleName.c_str(),
-                                                     nullptr);
-    if (handleStatus) {
-        out << "Create handle for '" << handleName << "' failed with: 0x" << std::hex << handleStatus << '\n';
-    }
-    return handle;
-}
-
-void releaseHandleExample(std::ostream& out, long port, const AmsAddr& server, uint32_t handle)
-{
-    const long releaseHandle = AdsSyncWriteReqEx(port, &server, ADSIGRP_SYM_RELEASEHND, 0, sizeof(handle), &handle);
-    if (releaseHandle) {
-        out << "Release handle 0x" << std::hex << handle << "' failed with: 0x" << releaseHandle << '\n';
-    }
-}
-
-void notificationExample(std::ostream& out, long port, const AmsAddr& server)
-{
-    const AdsNotificationAttrib attrib = {
-        1,
-        ADSTRANS_SERVERCYCLE,
-        0,
-        {4000000}
-    };
-    uint32_t hNotify;
-    uint32_t hUser = 0;
-
-    const long addStatus = AdsSyncAddDeviceNotificationReqEx(port,
-                                                             &server,
-                                                             0x4020,
-                                                             4,
-                                                             &attrib,
-                                                             &NotifyCallback,
-                                                             hUser,
-                                                             &hNotify);
-    if (addStatus) {
-        out << "Add device notification failed with: " << std::dec << addStatus << '\n';
-        return;
+    static void NotifyCallback(const AmsAddr* pAddr, const AdsNotificationHeader* pNotification, uint32_t hUser)
+    {
+        const uint8_t* data = reinterpret_cast<const uint8_t*>(pNotification + 1);
+        std::cout << "hUser 0x" << std::hex << hUser <<
+            " sample time: " << std::dec << pNotification->nTimeStamp <<
+            " sample size: " << std::dec << pNotification->cbSampleSize <<
+            " value:";
+        for (size_t i = 0; i < pNotification->cbSampleSize; ++i) {
+            std::cout << " 0x" << std::hex << (int)data[i];
+        }
+        std::cout << '\n';
     }
 
-    std::cout << "Hit ENTER to stop notifications\n";
-    std::cin.ignore();
+    namespace by_address
+    {
+        const uint32_t READ_WRITE_M_BASE = 0x4020;
+        void read(std::ostream& out, long port, const AmsAddr& server)
+        {
+            uint32_t bytesRead;
+            uint32_t buffer;
 
-    const long delStatus = AdsSyncDelDeviceNotificationReqEx(port, &server, hNotify);
-    if (delStatus) {
-        out << "Delete device notification failed with: " << std::dec << delStatus;
-        return;
+            out << __FUNCTION__ << "():\n";
+            for (size_t i = 0; i < 8; ++i) {
+                const long status = AdsSyncReadReqEx2(port,
+                    &server,
+                    READ_WRITE_M_BASE,
+                    0, // address of MAIN.byByte
+                    sizeof(buffer),
+                    &buffer,
+                    &bytesRead);
+                if (status) {
+                    out << "ADS read failed with: " << std::dec << status << '\n';
+                    return;
+                }
+                out << "ADS read " << std::dec << bytesRead << " bytes, value: 0x" << std::hex << buffer << '\n';
+            }
+        }
+
+        void notification(std::ostream& out, long port, const AmsAddr& server)
+        {
+            const AdsNotificationAttrib attrib = {
+                1,
+                ADSTRANS_SERVERCYCLE,
+                0,
+                { 4000000 }
+            };
+            uint32_t hNotify;
+            uint32_t hUser = 0;
+
+            const long addStatus = AdsSyncAddDeviceNotificationReqEx(port,
+                &server,
+                READ_WRITE_M_BASE,
+                4,
+                &attrib,
+                &NotifyCallback,
+                hUser,
+                &hNotify);
+            if (addStatus) {
+                out << "Add device notification failed with: " << std::dec << addStatus << '\n';
+                return;
+            }
+
+            std::cout << "Hit ENTER to stop notifications\n";
+            std::cin.ignore();
+
+            const long delStatus = AdsSyncDelDeviceNotificationReqEx(port, &server, hNotify);
+            if (delStatus) {
+                out << "Delete device notification failed with: " << std::dec << delStatus;
+                return;
+            }
+        }
     }
-}
 
-void notificationByNameExample(std::ostream& out, long port, const AmsAddr& server)
-{
-    const AdsNotificationAttrib attrib = {
-        1,
-        ADSTRANS_SERVERCYCLE,
-        0,
-        {4000000}
-    };
-    uint32_t hNotify;
-    uint32_t hUser = 0;
+    namespace by_name
+    {
+        struct auto_handle
+        {
+            auto_handle(std::ostream& out, long port, const AmsAddr& server,
+                const std::string name)
+                : m_out(out), m_port(port), m_server(server), m_name(name), m_handle(0)
+            {
+                const long handleStatus = AdsSyncReadWriteReqEx2(m_port,
+                    &m_server,
+                    ADSIGRP_SYM_HNDBYNAME,
+                    0,
+                    sizeof(m_handle),
+                    &m_handle,
+                    m_name.size(),
+                    m_name.c_str(),
+                    nullptr);
+                if (handleStatus) {
+                    m_out << "Create handle for '" << m_name << "' failed with: 0x" << std::hex << handleStatus << '\n';
+                }
+            }
 
-    uint32_t handle;
+            const uint32_t get() const
+            {
+                return m_handle;
+            }
 
-    out << __FUNCTION__ << "():\n";
-    handle = getHandleByNameExample(out, port, server, "MAIN.byByte");
+            ~auto_handle()
+            {
+                const long releaseHandle = AdsSyncWriteReqEx(m_port, &m_server, ADSIGRP_SYM_RELEASEHND, 0, sizeof(m_handle), &m_handle);
+                if (releaseHandle) {
+                    m_out << "Release handle 0x" << std::hex << m_handle << "' failed with: 0x" << releaseHandle << '\n';
+                }
+            }
+        private:
+            uint32_t m_handle;
+            long m_port;
+            AmsAddr m_server;
+            std::ostream& m_out;
+            const std::string m_name;
+        };
 
-    const long addStatus = AdsSyncAddDeviceNotificationReqEx(port,
-                                                             &server,
-                                                             ADSIGRP_SYM_VALBYHND,
-                                                             handle,
-                                                             &attrib,
-                                                             &NotifyCallback,
-                                                             hUser,
-                                                             &hNotify);
-    if (addStatus) {
-        out << "Add device notification failed with: " << std::dec << addStatus << '\n';
-        return;
+        void notification(std::ostream& out, long port, const AmsAddr& server)
+        {
+            const AdsNotificationAttrib attrib = {
+                1,
+                ADSTRANS_SERVERCYCLE,
+                0,
+                { 4000000 }
+            };
+            uint32_t hNotify;
+            uint32_t hUser = 0;
+
+            out << __FUNCTION__ << "():\n";
+            auto_handle handle(out, port, server, "MAIN.byByte");
+
+            const long addStatus = AdsSyncAddDeviceNotificationReqEx(port,
+                &server,
+                ADSIGRP_SYM_VALBYHND,
+                handle.get(),
+                &attrib,
+                &NotifyCallback,
+                hUser,
+                &hNotify);
+            if (addStatus) {
+                out << "Add device notification failed with: " << std::dec << addStatus << '\n';
+                return;
+            }
+
+            std::cout << "Hit ENTER to stop by name notifications\n";
+            std::cin.ignore();
+
+            const long delStatus = AdsSyncDelDeviceNotificationReqEx(port, &server, hNotify);
+            if (delStatus) {
+                out << "Delete device notification failed with: " << std::dec << delStatus;
+                return;
+            }
+        }
+
+        void read(std::ostream& out, long port, const AmsAddr& server)
+        {
+            uint32_t bytesRead;
+            uint32_t buffer;
+
+            out << __FUNCTION__ << "():\n";
+            auto_handle handle(out, port, server, "MAIN.byByte");
+
+            for (size_t i = 0; i < 8; ++i) {
+                const long status = AdsSyncReadReqEx2(port,
+                    &server,
+                    ADSIGRP_SYM_VALBYHND,
+                    handle.get(),
+                    sizeof(buffer),
+                    &buffer,
+                    &bytesRead);
+                if (status) {
+                    out << "ADS read failed with: " << std::dec << status << '\n';
+                    return;
+                }
+                out << "ADS read " << std::dec << bytesRead << " bytes, value: 0x" << std::hex << buffer << '\n';
+            }
+        }
+
     }
 
-    std::cout << "Hit ENTER to stop by name notifications\n";
-    std::cin.ignore();
 
-    const long delStatus = AdsSyncDelDeviceNotificationReqEx(port, &server, hNotify);
-    if (delStatus) {
-        out << "Delete device notification failed with: " << std::dec << delStatus;
-        return;
-    }
-    releaseHandleExample(out, port, server, handle);
-}
 
-void readExample(std::ostream& out, long port, const AmsAddr& server)
-{
-    uint32_t bytesRead;
-    uint32_t buffer;
 
-    out << __FUNCTION__ << "():\n";
-    for (size_t i = 0; i < 8; ++i) {
-        const long status = AdsSyncReadReqEx2(port, &server, 0x4020, 0, sizeof(buffer), &buffer, &bytesRead);
+
+    void readState(std::ostream& out, long port, const AmsAddr& server)
+    {
+        uint16_t adsState;
+        uint16_t devState;
+
+        const long status = AdsSyncReadStateReqEx(port, &server, &adsState, &devState);
         if (status) {
             out << "ADS read failed with: " << std::dec << status << '\n';
             return;
         }
-        out << "ADS read " << std::dec << bytesRead << " bytes, value: 0x" << std::hex << buffer << '\n';
+        out << "ADS state: " << std::dec << adsState << " devState: " << std::dec << devState << '\n';
     }
-}
 
-void readByNameExample(std::ostream& out, long port, const AmsAddr& server)
-{
-    uint32_t bytesRead;
-    uint32_t buffer;
-    uint32_t handle;
+    void run(const remote_target& target, std::ostream& out = std::cout)
+    {
 
-    out << __FUNCTION__ << "():\n";
-    handle = getHandleByNameExample(out, port, server, "MAIN.byByte");
 
-    for (size_t i = 0; i < 8; ++i) {
-        const long status = AdsSyncReadReqEx2(port,
-                                              &server,
-                                              ADSIGRP_SYM_VALBYHND,
-                                              handle,
-                                              sizeof(buffer),
-                                              &buffer,
-                                              &bytesRead);
-        if (status) {
-            out << "ADS read failed with: " << std::dec << status << '\n';
+        // add local route to your EtherCAT Master
+        if (AdsAddRoute(target.server.netId, target.ip_v4)) {
+            out << "Adding ADS route failed, did you specified valid addresses?\n";
             return;
         }
-        out << "ADS read " << std::dec << bytesRead << " bytes, value: 0x" << std::hex << buffer << '\n';
-    }
-    releaseHandleExample(out, port, server, handle);
-}
 
-void readStateExample(std::ostream& out, long port, const AmsAddr& server)
-{
-    uint16_t adsState;
-    uint16_t devState;
+        // open a new ADS port
+        const long port = AdsPortOpenEx();
+        if (!port) {
+            out << "Open ADS port failed\n";
+            return;
+        }
 
-    const long status = AdsSyncReadStateReqEx(port, &server, &adsState, &devState);
-    if (status) {
-        out << "ADS read failed with: " << std::dec << status << '\n';
-        return;
-    }
-    out << "ADS state: " << std::dec << adsState << " devState: " << std::dec << devState << '\n';
-}
+        const AmsAddr& server = target.server;
 
-void runExample(std::ostream& out)
-{
-    static const AmsNetId remoteNetId { 192, 168, 0, 231, 1, 1 };
-    static const char remoteIpV4[] = "192.168.0.232";
+        by_address::notification(out, port, server);
+        by_name::notification(out, port, server);
+        by_address::read(out, port, server);
+        by_name::read(out, port, server);
+        readState(out, port, server);
 
-    // add local route to your EtherCAT Master
-    if (AdsAddRoute(remoteNetId, remoteIpV4)) {
-        out << "Adding ADS route failed, did you specified valid addresses?\n";
-        return;
-    }
-
-    // open a new ADS port
-    const long port = AdsPortOpenEx();
-    if (!port) {
-        out << "Open ADS port failed\n";
-        return;
-    }
-
-    const AmsAddr remote { remoteNetId, AMSPORT_R0_PLC_TC3 };
-    notificationExample(out, port, remote);
-    notificationByNameExample(out, port, remote);
-    readExample(out, port, remote);
-    readByNameExample(out, port, remote);
-    readStateExample(out, port, remote);
-
-    const long closeStatus = AdsPortCloseEx(port);
-    if (closeStatus) {
-        out << "Close ADS port failed with: " << std::dec << closeStatus << '\n';
-    }
+        const long closeStatus = AdsPortCloseEx(port);
+        if (closeStatus) {
+            out << "Close ADS port failed with: " << std::dec << closeStatus << '\n';
+        }
 
 #ifdef _WIN32
-    // WORKAROUND: On Win7 std::thread::join() called in destructors
-    //             of static objects might wait forever...
-    AdsDelRoute(remoteNetId);
+        // WORKAROUND: On Win7 std::thread::join() called in destructors
+        //             of static objects might wait forever...
+        AdsDelRoute(target.server.netId);
 #endif
+    }
 }
 
 int main()
 {
-    runExample(std::cout);
+    // example for TWinCAT2
+    const remote_target target_tc2 = {
+        "10.8.9.36",                    // ip v4
+        {
+            { 10, 0, 96, 139, 1, 1 },   // AMS Net Id 
+            AMSPORT_R0_PLC              // AMS Port number
+        }
+    };
+
+    /*
+    // example for TWinCAT3
+    const remote_target target_tc3 = {
+        "192.168.0.232",                 // ip v4
+        {
+            { 192, 168, 0, 231, 1, 1 },  // AMS Net Id 
+            AMSPORT_R0_PLC_TC3           // AMS Port number
+        }
+    };
+    */
+
+    examples::run(target_tc2, std::cout);
 }
